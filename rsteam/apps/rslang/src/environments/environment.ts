0dec1231bf7125bcd5d017462f1536230cde1e97
sashtje <sashtje.lefevre@gmail.com>
2022-02-02 15:58:12 +0300

    init: start rslang

---
+++ rsteam/apps/rslang/src/environments/environment.ts
@@ -0,0 +1,8 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// When building for production, this file is replaced with `environment.prod.ts`.
+
+const environment = {
+  production: false,
+};
+
+export default environment;
