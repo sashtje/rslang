b6cf0a14933e4a98be56c651acf0e3cf1b5c16cf
sashtje <sashtje.lefevre@gmail.com>
2022-02-10 18:26:26 +0300

    feat: add layout and styles for login and sign up pages

--- rsteam/apps/rslang/src/pages/signup/SingUp.tsx
+++ rsteam/apps/rslang/src/pages/signup/SingUp.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Footer from '../../components/UI/footer/Footer';
+import { Link } from 'react-router-dom';
+import PasswordInput from '../../components/UI/password-input/PasswordInput';
 
 const SingUp = () => {
   return (
     <div className='container-wrapper'>
-      <main className='home main'>
-        <div className='container-inner'>
-          <h1>Sign up</h1>
+      <main className='login main'>
+        <div className='login__form-column'>
+          <form className='login__form login-form' action="">
+            <h1 className='login-form__title'>Регистрация</h1>
+            <p className='login-form__subtitle'>Заполни поля ниже, чтобы создать аккаунт RsLang</p>
+            <label className='login-form__label login-form__label_is_margin-top' htmlFor="name" >Имя</label>
+            <input className='login-form__input' type="text" id='name' autoComplete='' />
+            <label className='login-form__label' htmlFor="email">Email</label>
+            <input className='login-form__input' type="text" id='email' autoComplete='' />
+            <label className='login-form__label' htmlFor="password">Пароль</label>
+            <PasswordInput />
+            <p className='login-form__error'></p>
+            <button className='login-form__btn btn'  type='button'>Создать аккаунт</button>
+            <p className='login-form__question'>Уже с нами? <Link className='login-form__link' to='/login'>Войти в аккаунт</Link></p>
+          </form>
         </div>
+
+        <div className='login__picture'></div>
       </main>
       <Footer />
     </div>
